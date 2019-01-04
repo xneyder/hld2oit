ac93a2ee14f83ea1bfc757d23fe0e25d7c059f4d
Helix Application Mediation User6 <med06@tnknox01-hlxmed-06.uscc.com>
2019-01-04 11:15:56 -0600

    initiala commit

---
+++ hld2oit.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# hld2oit.py:
+#
+# Description: 	Tool intended to convert HLD format files to OIT format
+#       
+#		
+# Input Parameters:
+#		HLD File: Location to the HLD excel file
+#
+# Output: OIT excel file
+#
+# Example:
+#		hld2oit.py "HLD_USC_AFF_vMCC_V.1.0.2.xls"
+#
+# Database:	N/A
+#
+# Created by : Daniel Jaramillo
+# Creation Date: 04/01/2019
+# Modified by:     Date:
+# All rights(C) reserved to Teoco
+###########################################################################
+import sys
+import os
+import pandas as pd
+from openpyxl import load_workbook
+from LoggerInit import LoggerInit
+
+
+
